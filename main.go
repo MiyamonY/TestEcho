673832992155c6d1021e9a491cdc0826ab9e4e33
MIYAMOTO Yohei <y.miyamoto324@gmail.com>
2018-06-06 22:29:08 +0900

    middlewareを追加

--- main.go
+++ main.go
@@ -10,10 +10,14 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
 func main() {
 	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
 
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
